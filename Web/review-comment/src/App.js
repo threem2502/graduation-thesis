d09ee980dc725582166e209ef4e238f1e602cb91
Nguyen Duc Manh <nguyenducmanh@Manhs-Macbook-Pro.local>
2024-05-21 16:07:19 +0700

    build review AI

--- Web/review-comment/src/App.js
+++ Web/review-comment/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AppHeader from './components/AppHeader';
 import AppNavbar from './components/AppNavbar';
+import ReviewAIView from './components/ReviewAIView';
 
 function App() {
   return (
     <>
     <AppHeader/>
     <AppNavbar/>
+    <ReviewAIView/>
     </>
   );
 }
